30ff1c32fd72acebab7407531f8745b3633488f2
herald-noel <bontilao.heraldnoel@gmail.com>
2022-12-08 22:50:58 +0800

    Add and implement transfer method

--- src/BankTransfer.java
+++ src/BankTransfer.java
@@ -1,3 +1,9 @@
 public class BankTransfer {
-
+    public void transfer(Account from, Account to, double amount) throws Exception {
+        if (from.getBalance() < amount) {
+            throw new Exception("Insufficient funds");
+        }
+        from.withdraw(amount);
+        to.deposit(amount);
+    }
 }
